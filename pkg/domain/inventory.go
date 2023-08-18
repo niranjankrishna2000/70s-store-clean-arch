d0710828e55a8b1fe0fc3176321a1731aadff45c
niranjan2000 <niranjankrishna.thundathil@gmail.com>
2023-08-18 19:37:42 +0530

    added inventory description

--- pkg/domain/inventory.go
+++ pkg/domain/inventory.go
@@ -1,17 +1,18 @@
 package domain
 
-//Inventory represents the Products in the website
+// Inventory represents the Products in the website
 type Inventory struct {
 	ID          uint     `json:"id" gorm:"unique;not null"`
 	CategoryID  int      `json:"category_id"`
 	Category    Category `json:"-" gorm:"foreignkey:CategoryID;constraint:OnDelete:CASCADE"`
 	ProductName string   `json:"product_name"`
+	Description string   `json:"description"`
 	Image       string   `json:"image"`
 	Stock       int      `json:"stock"`
 	Price       float64  `json:"price"`
 }
 
-//Category represents the category of product
+// Category represents the category of product
 type Category struct {
 	ID       uint   `json:"id" gorm:"unique;not null"`
 	Category string `json:"category"`
