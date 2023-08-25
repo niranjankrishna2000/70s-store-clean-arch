c981b412e8a3cdcda8ff67c2b7ebf9d15d4c84e8
niranjan2000 <niranjankrishna.thundathil@gmail.com>
2023-08-25 18:27:34 +0530

    coupon bug final

--- pkg/usecase/order.go
+++ pkg/usecase/order.go
@@ -61,9 +61,9 @@ func (i *orderUseCase) OrderItemsFromCart(userid int, order models.Order, coupon
 		DiscountRate := i.couponRepo.FindCouponDiscount(coupon)
 
 		if DiscountRate > 0 {
-			total = total * float64((100-DiscountRate)/100)
-			fmt.Println("Discount", DiscountRate, "Total discount", (DiscountRate/100),int(total))
-			//total = total - float64(totalDiscount)
+			totalDiscount := total * float64(DiscountRate)/100.00
+			fmt.Println("Discount", DiscountRate, "Total discount", totalDiscount,(DiscountRate/100),int(total) ,int(DiscountRate/100))
+			total = total - totalDiscount
 		}
 	}
 
