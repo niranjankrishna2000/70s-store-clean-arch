b800e3228dfcdb4094b9ad752705953ba6256274
niranjan2000 <niranjankrishna.thundathil@gmail.com>
2023-08-25 19:48:48 +0530

    final 1.0

--- pkg/utils/models/inventory.go
+++ pkg/utils/models/inventory.go
@@ -2,28 +2,23 @@ package models
 
 type InventoryResponse struct {
 	ProductID int
-	Stock     int
-}
-
-type StockUpdate struct {
-	ID int `json:"id"`
-	//Stock     int `json:"stock"`
+	//Stock     int
 }
 
 type Inventory struct {
 	ID          uint    `json:"id"`
 	CategoryID  int     `json:"category_id"`
-	Image		string	`json:"image"`
+	Image       string  `json:"image"`
 	ProductName string  `json:"productName"`
-	Description string	`json:"description"`
+	Description string  `json:"description"`
 	Stock       int     `json:"stock"`
 	Price       float64 `json:"price"`
 }
 
-type UpdateInventory struct{
+type UpdateInventory struct {
 	CategoryID  int     `json:"category_id"`
 	ProductName string  `json:"productName"`
-	Description string	`json:"description"`
+	Description string  `json:"description"`
 	Stock       int     `json:"stock"`
 	Price       float64 `json:"price"`
 }
