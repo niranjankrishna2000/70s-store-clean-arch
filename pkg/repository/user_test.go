e8a5a60101d6efb5e8dcbec19c799b1ac829ff3a
niranjan2000 <niranjankrishna.thundathil@gmail.com>
2023-09-19 11:25:10 +0530

    test 2

--- pkg/repository/user_test.go
+++ pkg/repository/user_test.go
@@ -2,13 +2,19 @@ package repository
 
 import (
 	"errors"
+	"log"
+	"reflect"
 	"testing"
+
+	"main/pkg/utils/models"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-playground/assert/v2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+
 
 func Test_CheckUserAvailabilty(t *testing.T) {
 	tests := []struct {
@@ -81,39 +87,161 @@ func Test_CheckUserAvailabilty(t *testing.T) {
 
 }
 
-func Test_UserBlockStatus(t *testing.T){
-	tests:=[]struct{	
-		name string
-		args string
-		stub func(sqlmock.Sqlmock)
-		want bool
+func Test_UserBlockStatus(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    string
+		stub    func(sqlmock.Sqlmock)
+		want    bool
 		wantErr error
 	}{
 		{
 			name: "User is blocked",
 			args: "niranjan@gmail.com",
-			stub: func(mockSQL sqlmock.Sqlmock){
+			stub: func(mockSQL sqlmock.Sqlmock) {
 				expectedQuery := `^select permission from users(.+)$`
 
 				mockSQL.ExpectQuery(expectedQuery).
 					WillReturnRows(sqlmock.NewRows([]string{"permission"}).AddRow(true))
 			},
-			want: true,
+			want:    true,
 			wantErr: nil,
 		},
 		{
 			name: "user is not blocked",
 			args: "niranjan@gmail.com",
-			stub: func (mockSQL sqlmock.Sqlmock)  {
+			stub: func(mockSQL sqlmock.Sqlmock) {
 				expectedQuery := `^select permission from users(.+)$`
 
 				mockSQL.ExpectQuery(expectedQuery).WithArgs().WillReturnRows(sqlmock.NewRows([]string{"permission"}).AddRow(false))
 			},
-			want: false,
+			want:    false,
 			wantErr: nil,
 		},
 	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDB, mockSQL, _ := sqlmock.New()
+			defer mockDB.Close()
+
+			gormDB, _ := gorm.Open(postgres.New(postgres.Config{
+				Conn: mockDB,
+			}), &gorm.Config{})
+			tt.stub(mockSQL)
+			u := NewUserRepository(gormDB)
+			result, err := u.UserBlockStatus(tt.args)
+			assert.Equal(t, result, tt.want)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+func Test_SignUp(t *testing.T) {
+
+	type args struct {
+		input models.UserDetails
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		stub    func(sqlmock.Sqlmock)
+		want    models.UserResponse
+		wantErr error
+	}{
+		{
+			name: "success signup user",
+			args: args{
+				input: models.UserDetails{Name: "UserNew", Email: "usernew@gmail.com", Username: "usernew", Phone: "1234567890", Password: "userpass", ConfirmPassword: "userpass"},
+			},
+			
+			stub: func(mockSQL sqlmock.Sqlmock) {
+
+				expectedQuery := `^INSERT INTO users (.+)$`
+				mockSQL.ExpectQuery(expectedQuery).WithArgs("UserNew", "usernew@gmail.com","userpass","1234567890", "usernew").
+					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "phone"}).AddRow(1, "UserNew", "usernew@gmail.com","1234567890"))
+
+			},
+
+			want:    models.UserResponse{Id: 1, Name: "UserNew", Email: "usernew@gmail.com", Phone: "1234567890"},
+			wantErr: nil,
+		},
+
+		{
+			name: "error signup user",
+			args: args{
+				input: models.UserDetails{Name: "", Email: "", Username: "",Phone: "", Password: "",ConfirmPassword: ""},
+			},
+			stub: func(mockSQL sqlmock.Sqlmock) {
+
+				expectedQuery := `^INSERT INTO users (.+)$`
+				mockSQL.ExpectQuery(expectedQuery).WithArgs("", "", "", "", "").
+					WillReturnError(errors.New("Query 'INSERT INTO users (name, email,phone,password,username) VALUES ($1, $2, $3, $4,$5) RETURNING id, name, email, phone', arguments do not match: argument 4 expected [string - ] does not match actual [string - 12345]"))
+
+			},
+
+			want:    models.UserResponse{},
+			wantErr: errors.New("Query 'INSERT INTO users (name, email,phone,password,username) VALUES ($1, $2, $3, $4,$5) RETURNING id, name, email, phone', arguments do not match: argument 4 expected [string - ] does not match actual [string - 12345]"),
+		},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			mockDB, mockSQL, _ := sqlmock.New()
+			defer mockDB.Close()
+
+			gormDB, _ := gorm.Open(postgres.New(postgres.Config{
+				Conn: mockDB,
+			}), &gorm.Config{})
+
+			tt.stub(mockSQL)
+
+			u := NewUserRepository(gormDB)
+
+			got, err := u.SignUp(tt.args.input)
+			log.Println("log :",tt.name,got,tt.want)
+			log.Println("log :",tt.name,tt.wantErr,err)
+			assert.Equal(t, tt.wantErr, err)
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("userRepo.UserSignUp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func Test_FindUserByEmail(t *testing.T){
+	tests:=[]struct{
+		name string
+		args models.UserLogin
+		stub func(sqlmock.Sqlmock)
+		want models.UserResponse
+		wantErr error
+	}{
+		{
+			name: "User Found",
+			args: models.UserLogin{
+				Email: "niranjan@gmail.com",
+				Password: "niranjan2000",
+			},
+			stub: func(s sqlmock.Sqlmock) {
+				expectedQuery:=`^SELECT .* FROM users (.+)$`
+				s.ExpectQuery(expectedQuery).WithArgs("niranjan@gmail.com").WillReturnRows(sqlmock.NewRows([]string{"id", "name", "username","email", "phone", "password"}).AddRow(1,"Niranjan","niranjan","niranjan@gmail.com","8593008099","niranjan2000"))
+			},
+			want: models.UserResponse{Id: 1,
+				Name: "Niranjan",
+				Email: "niranjan@gmail.com",
+				Username: "niranjan",
+				Phone: "8593008099",
+				Password: "niranjan2000",
+				
+			},		
+			wantErr: nil,
+		},
+	}
 	for _,tt:=range tests{
 		t.Run(tt.name,func(t *testing.T) {
 			mockDB, mockSQL, _ := sqlmock.New()
@@ -122,11 +250,20 @@ func Test_UserBlockStatus(t *testing.T){
 			gormDB, _ := gorm.Open(postgres.New(postgres.Config{
 				Conn: mockDB,
 			}), &gorm.Config{})
-				tt.stub(mockSQL)
-				u:=NewUserRepository(gormDB)
-				result,err:=u.UserBlockStatus(tt.args)
-				assert.Equal(t,result,tt.want)
-				assert.Equal(t,tt.wantErr,err)
+
+			tt.stub(mockSQL)
+
+			u := NewUserRepository(gormDB)
+			got,err:=u.FindUserByEmail(tt.args)
+
+			log.Println("log :",tt.name,tt.want,got)
+			log.Println("log :",tt.name,tt.wantErr,err)
+
+			assert.Equal(t,tt.want,got)
+			assert.Equal(t,err,tt.wantErr)
 		})
+
+
 	}
+
 }
